63c051de121701f7ff38296c50fc457126ae3866
martinw-dev <martinwalsh.dev@gmail.com>
2022-08-26 21:35:44 +0100

    GET mapping and request path

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,20 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+    @GetMapping
+    public List<Student> getAllStudents() {
+        List<Student> students = Arrays.asList(
+                new Student(1L, "Martin", "martin@gmail.com", Gender.MALE),
+                new Student(1L, "Jane", "jane@gmail.com", Gender.FEMALE));
+        return students;
+    }
+}
