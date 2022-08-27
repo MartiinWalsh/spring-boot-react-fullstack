81a13ca5712748c73050611d1f6ceee8d14c6957
martinw-dev <martinwalsh.dev@gmail.com>
2022-08-27 10:40:17 +0100

    State management

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,32 @@
+import {useEffect, useState} from "react";
 import {getAllStudents} from "./client";
 
 import './App.css';
 
 
 function App() {
-    getAllStudents().then(response => response.data).then(console.log);
-    return "";
+    const [students, setStudents] = useState([]);
+
+    const fetchStudents = () =>
+        getAllStudents()
+            .then(response => response.data)
+            .then(student_data => {
+                console.log(student_data)
+                setStudents(student_data)
+            });
+
+    useEffect(() => {
+        console.log("component is mounted");
+        fetchStudents();
+    }, []);
+
+    if (students.length <= 0) {
+        return 'No data';
+    } else {
+        return students.map((student, index) => {
+            return <p key={index}>{student.id} {student.name}</p>
+        })
+    }
 }
 
 export default App;
