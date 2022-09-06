24a2b8811018495a4c4b3772e0d574df193cc9fe
martinw-dev <martinwalsh.dev@gmail.com>
2022-09-06 20:48:40 +0100

    N Tier architecture

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,20 +1,20 @@
 package com.example.demo.student;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
+@AllArgsConstructor
 public class StudentController {
+    private final StudentService studentService;
+
     @GetMapping
     public List<Student> getAllStudents() {
-        List<Student> students = Arrays.asList(
-                new Student(1L, "Martin", "martin@gmail.com", Gender.MALE),
-                new Student(2L, "Jane", "jane@gmail.com", Gender.FEMALE));
-        return students;
+        return studentService.getAllStudents();
     }
 }
